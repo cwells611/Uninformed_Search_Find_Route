6f202f2fba719dd8a1ee947280a0f2c8d8e78307
Caden <cwells@mines.edu>
2024-04-20 11:53:01 -0600

    parsed through input file specified from command line and created list os lists where each sub list contains the elements of each line of the input file, will use this list to create graph

--- find_route.py
+++ find_route.py
@@ -1,4 +1,30 @@
 import networkx as nx
+import sys
 
 #create new graph to replicate the input city map 
 cities = nx.Graph()
+
+#open file specified from command line 
+input_file = open(sys.argv[1], "r")
+
+#create list of lists where each sub list contains the elements of a line of the input
+input_lines = input_file.readlines(); 
+input_array = []
+for line in input_lines:
+    line = line.strip()
+    #check for end of input 
+    if line == "END OF INPUT":
+        break; 
+    individual_line = []
+    elements = line.split()
+    for element in elements:
+        individual_line.append(element)
+    input_array.append(individual_line)
+
+
+#loop through the input file and add nodes, edges, 
+#and weights to city graph 
+
+
+#close file
+input_file.close()
